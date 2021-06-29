67f3fbd8cffb0a2a306fdd24ec28ded7a6680e40
Lucas Bacelo <bacelolucas@gmail.com>
2021-06-29 00:24:14 -0300

    Refactor clase 5

--- A. Aprendiendo Go/Clase 5/main.go
+++ A. Aprendiendo Go/Clase 5/main.go
@@ -1,21 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
+	// En Go solo existe el bucle for el cual se utiliza para repetir
+	// un bloque de código una determinada cantidad de veces
+
+	vueltas := 10
+
+	// El primer término es el inicializador. En este caso crea una variable 'v' con el valor 0.
+	// El segundo término es la condición. En este caso la condición es que de 'v' sea menor que el valor 'vueltas'.
+	// El tercer término es la acción si se cumple la condición. En este caso se incrementa 1 el valor de 'v'.
+	for v := 0; v < vueltas; v++ {
+		fmt.Println("Vuelta: ", v)
+	}
+
+	// Se pueden omitir el bloque de inicialización y de acción, dejando solo el de condición.
+	// De esta manera, el bucle for se comporta como un bucle while, el cual se utiliza para repetir
+	// un bloque de código siempre que la condición sea verdadera
+
+	condicion := true
+
 	contador := 0
-	condicion := false
 
 	// While
 	for condicion == true {
-		// Hago algo
-
-		contador++
-		if contador == 10 {
-			condicion = true
+		if contador == 10 { // Si el contador llega a 10, la condición se hace false
+			condicion = false
+		} else {
+			contador++
 		}
 	}
 
-	// For
-	for i := 0; i < 10; i++ {
-		// Hago algo
+	// En Go existe la palabra clave 'range' la cual se utiliza para obtener el índice y el valor de los valores
+	// de vector o mapa de manera secuencial.
+
+	frutas := []string{"banana", "manzana", "pera"}
+
+	for indice, nombre := range frutas {
+		fmt.Println(indice, nombre)
 	}
 }
